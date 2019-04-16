1577b04ce1aafc19f692492e4cf1ba347cf88742
Maksym Pushkarov <maksym_pushkarov@epam.com>
2019-04-16 22:21:28 +0300

    Animated image

--- game/js/main.js
+++ game/js/main.js
@@ -2,26 +2,23 @@
   const canvas = document.querySelector('.root');
 
   const context = canvas.getContext('2d');
-
-  context.strokeText('Rectangle', 10, 10)
-  context.strokeStyle = 'green';
-  context.fillStyle = 'blue';
-  context.shadowBlur = 10;
-  context.shadowColor = 'brown';
-
-  context.rect(10, 30, 80, 40);
-  context.fill();
-  context.stroke();
-
-
-
-  context.strokeText('Crankle', 150, 10);
-  context.moveTo(150, 40);
-  const x = [150, 180, 210, 240, 270, 300];
-  const y = [40, 80, 40, 80, 40, 80];
-  for (let i = 0, length = x.length; i < length; i++) {
-    context.lineTo(x[i], y[i]);
+  const image = new Image();
+  let interval;
+  image.onload = function() {
+    context.drawImage(image, 10, 10, 32, 32);
+    interval = setInterval(move, 200)
   }
-  context.stroke();
 
+  let  x = 10;
+  function move () {
+    if (x < 400) {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.drawImage(image, x, 10, 32, 32);
+      x += 10;
+    } else {
+      x = 10;
+    }
+  }
+
+  image.src = './images/smile.png';
 })();
